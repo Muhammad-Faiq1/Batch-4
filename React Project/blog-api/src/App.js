b2372764ffa01f733254648a86b69fd1132b01b8
Muhammad Faiq <faiqkhan941@gmail.com>
2023-02-18 19:31:32 +0500

    react

---
+++ React Project/blog-api/src/App.js
@@ -0,0 +1,13 @@
+
+import Login from './AntdComponets/Login/Login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
